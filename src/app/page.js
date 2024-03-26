d6d0f1dda22b84b3c7b665e72c0a34cf69e7e2ee
Robin Khuc <robin.khuc@beyondedge.co>
2024-03-26 15:11:38 +0700

    add home header component

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,10 @@
+import HomeHeader from '@/components/HomeHeader';
+
 export default function Home() {
-  return <main className=''>Home Page</main>;
+  return (
+    <>
+      <HomeHeader />
+      <main className=''></main>
+    </>
+  );
 }
