21226b3250285999a11a6e0bc3eef45c44ab42ed
Robin Khuc <robin.khuc@beyondedge.co>
2024-03-27 13:34:28 +0700

    create search result component

--- src/app/search/web/page.js
+++ src/app/search/web/page.js
@@ -1,3 +1,4 @@
+import WebSearchResults from '@/components/WebSearchResults';
 import Link from 'next/link';
 import { redirect } from 'next/navigation';
 
@@ -31,7 +32,5 @@ export default async function WebSearchPage({ searchParams }) {
       </div>
     );
 
-  return (
-    <div>{results && results.map((result) => <h1>{result.title}</h1>)}</div>
-  );
+  return <div>{results && <WebSearchResults results={data} />}</div>;
 }
