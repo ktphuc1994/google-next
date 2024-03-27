e01475e100994b199a60b2bc550b9365a23df2eb
Robin Khuc <robin.khuc@beyondedge.co>
2024-03-27 16:04:25 +0700

    add loading effect for web and image page

--- src/app/search/error.js
+++ src/app/search/error.js
@@ -10,7 +10,14 @@ export default function SearchError({ error, reset }) {
   return (
     <div className='flex flex-col justify-center items-center pt-10'>
       <h1 className='text-3xl mb-4'>Something went wrong</h1>
-      <button className='text-blue-500'>Try again</button>
+      <button
+        className='text-blue-500'
+        onClick={() => {
+          reset();
+        }}
+      >
+        Try again
+      </button>
     </div>
   );
 }
--- src/app/search/image/page.js
+++ src/app/search/image/page.js
@@ -7,6 +7,10 @@ export default async function ImageSearchPage({ searchParams }) {
     redirect('/');
   }
 
+  await new Promise((resolve) => {
+    setTimeout(resolve, 1000);
+  });
+
   const response = await fetch(
     `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchTerm}&searchType=image&start=${startIndex}`
   );
